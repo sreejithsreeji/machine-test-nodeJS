2897b9a94737b8fc962a71051f5dd6eb4dbc433e
sreejith <m.srijithsri@gmail.com>
2019-08-02 21:47:23 +0530

    bug fixed

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const userModel=require('../models/user.js');
 
 const register=(req,res)=>{
     
-    userModel.register({ ...req.body,image:req.file.path})
+    userModel.register({ ...req.body,image:req.file?req.file.path:null})
     .then(response=>{
         res.status(201)
         .send({
